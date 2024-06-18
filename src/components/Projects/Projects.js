6dffc431d3ce4d1d825b45f5f2822b01a4c19bde
Akshad Gawde <gawdeakshad@gmail.com>
2024-06-18 11:47:57 +0530

    updated projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,6 +8,11 @@ import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
+import nmims from "../../Assets/Projects/nmims.png";
+import DD from "../../Assets/Projects/DD.png";
+import ECELL from "../../Assets/Projects/ECELL logo.svg";
+import Pinnacle from "../../Assets/Projects/Pinncale-logo.webp";
+import Adv from "../../Assets/Projects/Adv.jpeg";
 
 function Projects() {
   return (
@@ -34,12 +39,23 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={Pinnacle}
               isBlog={false}
               title="ClassHub"
               description="Designed and developed a dynamic and engaging website for a Mumbai-based educational institution, utilizing the versatility of React.js. This project not only underscores my expertise in crafting intuitive user interfaces but also emphasizes the tailored solutions provided for the specific needs of the classes."
               ghLink="https://github.com/AkshadGawde"
-              demoLink="-"
+              demoLink="https://pinnaclegrouptuitions.in"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Adv}
+              isBlog={false}
+              title="ClassHub"
+              description="Designed and developed a dynamic and engaging website for a Mumbai-based educational institution, utilizing the versatility of React.js. This project not only underscores my expertise in crafting intuitive user interfaces but also emphasizes the tailored solutions provided for the specific needs of the classes."
+              ghLink="https://github.com/AkshadGawde"
+              demoLink="https://advclasses.com"
             />
           </Col>
 
@@ -61,29 +77,40 @@ function Projects() {
               title="Portfolio Website"
               description="Crafted a personalized and visually stunning portfolio website for a friend using vanilla JavaScript, HTML, and CSS. The website showcases their unique skills, projects, and personality, providing a standout online presence to leave a lasting impression."
               ghLink="https://github.com/AkshadGawde/Shivansh_Web"
-              demoLink="https://portfolio-gamma-dusky-39.vercel.app/"
+              demoLink="https://shivansh2508.in"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={ECELL}
               isBlog={false}
-              title="Event App"
-              description="Developed an innovative Event Web App using Next.js, a React-based framework, to provide users with a seamless and engaging platform for discovering upcoming events. This dynamic application not only showcases a visually appealing display of scheduled events but also offers an intuitive and user-friendly experience. Leveraging Next.js allowed for efficient routing, rendering, and state management, resulting in a fast and responsive web app."
-              ghLink="https://github.com/AkshadGawde/Event-app"
-              demoLink="-"
-              include
-              a
-              demo
-              link
-              here
+              title="E-cell Nmims"
+              description="Developed a user-friendly website for E-cell at NMIMS Navi Mumbai which provides
+comprehensive information about the committee and its events. Optimized website performance and accessibility, adhering to web standards and usability
+guidelines."
+              ghLink="https://github.com/AkshadGawde/E-CELL_NMIMS.git"
+              demoLink="ecell.akshad.in/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={nmims}
+              isBlog={false}
+              title="NMIMS Portal"
+              description="A robust college management system and a scalable database database using Firestore Cloud,
+serving as a centralized platform for faculty and student’s needs. Implemented a seamless sign-up/sign-in functionalities and personalized dashboards also
+integrated the database to efficiently manage and display academic information, enhancing
+communication and workflow between faculty and students. "
+              ghLink="https://github.com/AkshadGawde/NMIMS-Student-Portal.git"
+              demoLink="https://portal.prabir.in"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={DD}
               isBlog={false}
               title="Freelance Website"
               description="Collaborated with my friend to craft a dynamic freelance website that showcases our combined expertise in web development. Leveraged HTML for structure, CSS for sleek and responsive styling, and JavaScript to bring interactive elements to life. To streamline our workflow and optimize performance, we employed the powerful Parcel bundler. "
